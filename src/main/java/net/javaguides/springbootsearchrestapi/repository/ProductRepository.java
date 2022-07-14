eacb113fe9629a5f19aee55d9135b289dba42f56
YEGON <kptooman@gmail.com>
2022-07-14 14:56:09 +0300

    first commit

---
+++ src/main/java/net/javaguides/springbootsearchrestapi/repository/ProductRepository.java
@@ -0,0 +1,22 @@
+package net.javaguides.springbootsearchrestapi.repository;
+
+import net.javaguides.springbootsearchrestapi.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+//    jplquery
+    @Query("SELECT p FROM Product p where " +
+            "p.name LIKE CONCAT('%',:query, '%')" +
+            "Or p.description LIKE CONCAT('%', :query, '%')")
+    List<Product> searchProducts(String query);
+
+//    native sql query
+    @Query( value = "SELECT * FROM products p where " +
+            "p.name LIKE CONCAT('%',:query, '%')" +
+            "Or p.description LIKE CONCAT('%', :query, '%')", nativeQuery = true)
+    List<Product> searchProductsSQL(String query);
+}
